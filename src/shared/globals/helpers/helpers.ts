1d0027b629c4c6426646a19031347e0b2871775b
Livia328 <374606755@qq.com>
2022-11-14 15:15:40 -0800

    feat: add user features

--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -36,15 +36,15 @@ export class Helpers {
     return dataUrlRegex.test(value);
   }
 
-  // static shuffle(list: string[]): string[] {
-  //   for (let i = list.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     [list[i], list[j]] = [list[j], list[i]];
-  //   }
-  //   return list;
-  // }
+  static shuffle(list: string[]): string[] {
+    for (let i = list.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list;
+  }
 
-  // static escapeRegex(text: string): string {
-  //   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-  // }
+  static escapeRegex(text: string): string {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
 }

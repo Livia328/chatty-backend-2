bb3aa0e2e647913e8865849d85e385e5cfc34485
Livia328 <374606755@qq.com>
2022-11-15 11:34:46 -0800

    created user seed script and set up npm command

--- src/features/user/routes/healthRoutes.ts
+++ src/features/user/routes/healthRoutes.ts
@@ -49,22 +49,22 @@ class HealthRoutes {
       const start: number = performance.now();
       const result: number = this.fibo(parseInt(num, 10));
       const end: number = performance.now();
-      const response = await axios({
-        method: 'get',
-        url: config.EC2_URL
-      });
-      res
-        .status(HTTP_STATUS.OK)
-        .send(
-          `Fibonacci series of ${num} is ${result} and it took ${end - start}ms and runs with process id ${process.pid} on ${
-            response.data
-          } at ${moment().format('LL')}`
-        );
+      // const response = await axios({
+      //   method: 'get',
+      //   url: config.EC2_URL
+      // });
       // res
-      // .status(HTTP_STATUS.OK)
-      // .send(
-      //   `Fibonacci series of ${num} is ${result} and it took ${end - start}ms and runs with process id ${process.pid} on ${moment().format('LL')}`
-      // );
+      //   .status(HTTP_STATUS.OK)
+      //   .send(
+      //     `Fibonacci series of ${num} is ${result} and it took ${end - start}ms and runs with process id ${process.pid} on ${
+      //       response.data
+      //     } at ${moment().format('LL')}`
+      //   );
+      res
+      .status(HTTP_STATUS.OK)
+      .send(
+        `Fibonacci series of ${num} is ${result} and it took ${end - start}ms and runs with process id ${process.pid} on ${moment().format('LL')}`
+      );
     });
 
     return this.router;
